51cbdd4d552da35f83eb9b47b621a114ff95118d
DreSean-Sutton <dreseansutton@gmail.com>
2022-07-29 18:52:28 -0700

    Checked out the difference/similarities between Ruby's and JavaScript's array methods

--- test.rb
+++ test.rb
@@ -55,9 +55,9 @@ BEGIN {
 class Customer
   @@no_of_customers = 0
   def initialize(id, name, addr)
-    @cust_id = id
-    @cust_name = name
-    @cust_addr = addr
+    @cust_id = id #id is a local variable
+    @cust_name = name #name is a local variable
+    @cust_addr = addr #addr is a local variable
   end
   def display_details
     puts "My id is: #@cust_id"
@@ -94,8 +94,52 @@ class Class2
 end
 
 class1obj = Class1.new
-class1obj.print_global #10. Global variable not touched, yet
+class1obj.print_global # 10. Global variable not touched, yet
 class2obj = Class2.new
 class2obj.print_global # 15. Global variable incremented
 class1obj2 = Class2.new
-class1obj2.print_global #20. Global variable still incremented for all
+class1obj2.print_global # 20. Global variable still incremented for all
+
+class ConstantExample
+  VAR1 = 100 # Constant variables work anywhere in class. Global if use outside Class.
+  VAR2 = 200 # Constant variables work anywhere in class. Global if use outside Class.
+  def show
+    puts "Value of first Constant is #{VAR1}"
+    puts "Value of second Constant is #{VAR2}"
+  end
+end
+
+constant1 = ConstantExample.new
+constant1.show
+
+# SOME ARRAY METHODS BELOW
+
+arr = ["elem1", 10, 51, 3.14, "elem5"];
+arr.each do |i| #arr value
+  puts i
+end
+
+arr.each_with_index do |n, idx| #arr value and index
+  puts "#{n} is item #{idx}"
+end
+
+prices = [10, 15, 55, 5, 20]
+
+double_all = prices.map do |n| n * 2 end
+puts "#{double_all}"
+
+filter_low_prices = prices.filter do |n| n >= 20 end
+puts "All high prices are: #{filter_low_prices}"
+
+arr = Array(0..10)
+
+n = arr.length / 2
+
+n.times do
+  new_last_value = arr.shift
+  arr.push new_last_value
+end
+puts "#{arr}" # [5, 6, 7, 8, 9, 10, 0, 1, 2, 3, 4]
+
+total_of_arr = arr.reduce(0) do |ac, cv| ac + cv end
+puts "total_of_arr is: #{total_of_arr}"
